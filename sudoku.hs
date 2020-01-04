127d47dbd7f2b3b56470009bfcda4de6654154c1
Lunaticorvus <Lunaticorvus@gmail.com>
2020-01-04 18:16:30 +0900

    Revised by Nuri.Park

--- sudoku.hs
+++ sudoku.hs
@@ -3,8 +3,9 @@ import Data.List
 
 main = do
     contents <- readFile "input.txt"
-    putStr $ unlines (map (intersperse ' ') ([(" " ++ ['1'..'9'])] ++ (attachHead (pred 'A') (lines contents))))
-    
-attachHead :: Char -> [[Char]] -> [[Char]]
-attachHead _ [] = []
-attachHead prefix (x:xs) = [succ prefix : x] ++ (attachHead (succ prefix) xs)
+    -- putStr $ unlines (map (intersperse ' ') ([(" " ++ ['1'..'9'])] ++ (attachHead (pred 'A') (lines contents))))
+    putStrLn $ unlines $ map (intersperse ' ') $ map (\(x, y) -> x:y) $ zip ['A'..] $ lines contents
+
+-- attachHead :: Char -> [[Char]] -> [[Char]]
+-- attachHead _ [] = []
+-- attach/Head prefix (x:xs) = [succ prefix : x] ++ (attachHead (succ prefix) xs)
