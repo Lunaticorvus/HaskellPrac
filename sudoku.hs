2e8be9247112dda36e6eb8cb080a531de18a7175
Minyoung Kwak <minyoung.kwak@1011.co.kr>
2020-01-06 09:02:49 +0900

    check 3by3

--- sudoku.hs
+++ sudoku.hs
@@ -7,30 +7,20 @@ main = do
     -- putStr $ unlines (map (intersperse ' ') ([(" " ++ ['1'..'9'])] ++ (attachHead (pred 'A') (lines contents))))
     putStrLn $ unlines $ map (intersperse ' ') $ map (\(x, y) -> x:y) $ zip ['A'..] $ lines contents
 
-    let row = 4
+    let row = 0
     let column = 0
-    let input = '4'
+    let input = '7'
     let board = lines contents
 
-    print $ [foldl (\acc x -> acc ++ x) [] (map ((take 3)) (take 3 board))]
-    print $ drop 1 ["1"]
-    --print $ maek3X3Board board
-    print $ (row `div` 3)
+    print $ (map (take 3) (take 3 (map (drop (row - (row `mod` 3))) (drop (column - (column `mod` 3)) (lines contents)))))
+
     -- can row 
     print $ input `elem` (filter (/='_') ((lines contents) !! row))
     -- can column
     print $ input `elem` ((filter (/='_')) ((transpose $ lines contents) !! row))
+    -- can 3by3
+    print $ input `elem` (filter (/='_') (foldl (\acc x -> acc ++ x) [] (map (take 3) (take 3 (map (drop (row - (row `mod` 3))) (drop (column - (column `mod` 3)) (lines contents)))))))
 
 -- attachHead :: Char -> [[Char]] -> [[Char]]
 -- attachHead _ [] = []
 -- attach/Head prefix (x:xs) = [succ prefix : x] ++ (attachHead (succ prefix) xs)
-
-maek3X3Board :: [[Char]] -> [[Char]]
-maek3X3Board [] = []
-maek3X3Board [[]] = [[]]
-maek3X3Board board = make3x3Piece $ (take 3 board) ++ (maek3X3Board $ (drop 3 board))
-
-make3x3Piece :: [[Char]] -> [[Char]]
-make3x3Piece [] = []
-make3x3Piece [[]] = [[]]
-make3x3Piece piece = [foldl (\acc x -> acc ++ x) [] (map ((take 3)) (take 3 piece))] ++ (make3x3Piece $ map (drop 3) piece)
