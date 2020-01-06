52637de98be729b3bc3bb7f2ed058338ec1f621b
Minyoung Kwak <minyoung.kwak@1011.co.kr>
2020-01-06 16:56:33 +0900

    validation function

--- sudoku.hs
+++ sudoku.hs
@@ -1,26 +1,31 @@
 import System.IO
 import Data.List
 import Data.Char
+import Control.Applicative
+import Data.List.Split
+
 
 main = do
     contents <- readFile "input.txt"
     -- putStr $ unlines (map (intersperse ' ') ([(" " ++ ['1'..'9'])] ++ (attachHead (pred 'A') (lines contents))))
-    putStrLn $ unlines $ map (intersperse ' ') $ map (\(x, y) -> x:y) $ zip ['A'..] $ lines contents
+    putStrLn $ unlines $ map (intersperse ' ') $ map (\(x, y) -> x:y) $ zip ['A'..] $ lines contents 
 
-    let row = 0
-    let column = 0
-    let input = '7'
-    let board = lines contents
+    print $ valid $ (lines contents)
 
-    print $ (map (take 3) (take 3 (map (drop (row - (row `mod` 3))) (drop (column - (column `mod` 3)) (lines contents)))))
+generate f = chunksOf 9 $ liftA2 f [0..8] [0..8]
+validChunk d c = (\x -> (length x) == (length $ nub x) ) $ filter (/= '_') $ map (\(i, j) -> d !! i !! j) c
+validChunks d cs = foldl1 (&&) $ map (validChunk d) cs
+valid1 d = validChunks d $ generate (,)  
+valid2 d = validChunks d $ generate (flip (,))
+valid3 d = validChunks d $ generate (\i j -> ((i `div` 3) * 3 + (j `div` 3), (i `mod` 3) * 3 + (j `mod` 3)))
+valid d = valid1 d && valid2 d && valid3 d
 
     -- can row 
-    print $ input `elem` (filter (/='_') ((lines contents) !! row))
+    --print $ input `elem` (filter (/='_') ((lines contents) !! row))
     -- can column
-    print $ input `elem` ((filter (/='_')) ((transpose $ lines contents) !! row))
+    --print $ input `elem` ((filter (/='_')) ((transpose $ lines contents) !! row))
     -- can 3by3
-    print $ input `elem` (filter (/='_') (foldl (\acc x -> acc ++ x) [] (map (take 3) (take 3 (map (drop (row - (row `mod` 3))) (drop (column - (column `mod` 3)) (lines contents)))))))
-
+    --print $ input `elem` (filter (/='_') (foldl (\acc x -> acc ++ x) [] (map (take 3) (take 3 (map (drop (row - (row `mod` 3))) (drop (column - (column `mod` 3)) (lines contents)))))))
 -- attachHead :: Char -> [[Char]] -> [[Char]]
 -- attachHead _ [] = []
 -- attach/Head prefix (x:xs) = [succ prefix : x] ++ (attachHead (succ prefix) xs)
